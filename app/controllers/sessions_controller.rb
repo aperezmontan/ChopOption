99c68bbd02e45ff9a35a27ee4951cd70075ebc77
Ari Perez <aris.a.perez@gmail.com>
2015-07-21 01:25:22 -0400

    Adds font-awesome

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,15 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if (user && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Either username or password are incorrect."
+      redirect_to login_path
+    end
+  end
+
 end
